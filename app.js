f8ee5a27d76b3e87a723ecd0fa7cfcaf76e80cf7
JonanLowe <jonathan_a_lowe@hotmail.com>
2024-10-14 14:39:02 +0100

    update app.js file for endpoint /api

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 const express = require("express");
 const app = express();
+const endpoints = require("./endpoints.json")
 
-const { getTopics } = require("./controllers/topics-controllers");
+const { getTopics, getApi } = require("./controllers/topics-controllers");
 
 app.get("/api/topics", getTopics);
+app.get("/api", (request, response) => {
+    response.status(200).send({ endpoints })
+});
+
+
+
 
 app.all("/*", (request, response) => {
   response.status(404).send({ msg: "Not Found" });
 });
 
-app.use((err, request, response, next) => {
+app.use((err, request, response) => {
   console.log("reached 500 server error");
   response.status(500).send({ msg: "Internal Server Error" });
 });
