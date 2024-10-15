52e69df18d27d8bc4caa6ab14b102054673b1582
JonanLowe <jonathan_a_lowe@hotmail.com>
2024-10-15 14:51:06 +0100

    6: add comments-models.js and complete for for /api/articles/:article_id/comments

--- models/comments-models.js
+++ models/comments-models.js
@@ -1,16 +1,21 @@
 const db = require("../db/connection");
 
-exports.countCommentsByArticle_id = (article_id) => {
+exports.selectCommentsByArticleId = (article_id) => {
   return db
     .query(
-    `
-    SELECT * FROM comments
-    WHERE article_id = $1;
-    `,
+      `SELECT *
+      FROM comments
+      WHERE article_id = $1
+      ORDER BY created_at DESC;`,
       [article_id]
     )
-    .then((result) => {
-      console.log(result.rowCount, "<<result.rowCount")
-      return result.rowCount;
+    .then((results) => {
+      if (results.rowCount < 1) {
+        return Promise.reject({
+          status: 404,
+          msg: "No comments available: Article does not exist",
+        });
+      }
+      return results.rows;
     });
 };
