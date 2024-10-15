50899e735a9182f4e8ecebcc3c6550d51441b176
JonanLowe <jonathan_a_lowe@hotmail.com>
2024-10-15 17:13:08 +0100

    task 6: update comments-models to handle 200 instead of 404

--- models/comments-models.js
+++ models/comments-models.js
@@ -1,21 +1,15 @@
 const db = require("../db/connection");
 
 exports.selectCommentsByArticleId = (article_id) => {
-  return db
-    .query(
-      `SELECT *
-      FROM comments
-      WHERE article_id = $1
-      ORDER BY created_at DESC;`,
-      [article_id]
-    )
-    .then((results) => {
-      if (results.rowCount < 1) {
-        return Promise.reject({
-          status: 404,
-          msg: "No comments available: Article does not exist",
-        });
-      }
-      return results.rows;
-    });
+
+  return db.query(
+    `SELECT *
+     FROM comments
+    WHERE article_id = $1
+    ORDER BY created_at DESC;`,
+    [article_id]
+  )
+  .then((results) => {
+    return results.rows;
+  });
 };
